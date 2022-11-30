88c9116961ddbb4fea2c4b85260815694f599756
Kenechukwu881 <akabuezeobumneme@gmail.com>
2022-11-30 14:27:47 -0500

    initial commit

--- ken_cdk_project/ken_cdk_project_stack.py
+++ ken_cdk_project/ken_cdk_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     # Duration,
+    aws_s3 as _s3,
     Stack,
     # aws_sqs as sqs,
 )
@@ -11,6 +12,15 @@ class KenCdkProjectStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "myBucketid",
+            bucket_name="myfirstcdkprojectbucket1001",
+            versioned=True,
+            # encryption=_s3.BucketEncryption.KMS
+
+        )
 
         # example resource
         # queue = sqs.Queue(
